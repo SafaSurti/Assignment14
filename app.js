6b086a0c9fe030d2da65625098c6b8d1bf19ac47
SafaSurti <safamushtaqsurti@gmail.com>
2024-09-12 17:32:38 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,201 @@
+// MATH EXPRESSION CHAPTER 5
+// QUESTION NO 1
+// var a =(3);
+// var b =(5);
+// document.write(a + b);
+// QUESTION NO 2(a)
+// var a =(3);
+// var b =(5);
+// document.write(a - b);
+// QUESTION NO 2(b)
+// var a =(3);
+// var b =(5);
+// document.write(a * b);
+// QUESTION NO 2(c)
+// var a =(3);
+// var b =(5);
+// document.write(a / b);
+// QUESTION NO 2(d)
+//  var a =(3);
+//  var b =(5);
+//  document.write(a % b);
+// QUESTION NO 3(a)
+// var  num =10;
+// console.log("Value after variable declaration is ; " + num)
+// document.write("Value after variable declaration is ; " +  num)
+// QUESTION NO 3(b)
+// var num =(10);
+// document.write("Initial value; " + num)
+// QUESTION NO 3(c)
+// var num =(5);
+// ++num;
+// document.write("Value after increment is; " + num);
+// QUESTION NO 3(d)
+// var num =(6);
+// num +=7;
+// document.write("Value after addition is; " + num);
+// QUESTION NO 3(e)
+//  var num =(13);
+//  --num;
+//  document.write("Value after deccrement is; " + num);
+// QUESTION NO 3(k)
+// var a =(6);
+// var b =(3);
+// document.write(a % b)
+// var num =(5);
+// ++num;
+// document.write("Vlaue after increment is; " + num)
+// var num =(6);
+// num+=7;
+// document.write("Value after addition is; " + num)
+// var num =(13);
+//  --num;
+//  document.write("Value after decrement is; " + num)
+// QUESTION NO 4
+// var ticketPrice =600;
+// ticketPrice*=5
+// document.write("Total cost to buy a 5 tickets to a movie is; " + ticketPrice)
+// QUESTION NO 5
+// document.write("Table of 4")
+// var multi =(4)
+// multi*=1
+// document.write("<br>4x1= " + multi)
+// var multi =(4)
+// multi*=2
+// document.write("<br>4x2= " + multi)
+// var multi =(4)
+// multi*=3
+// document.write("<br>4x3= " + multi)
+// var multi =(4)
+// multi*=4
+// document.write("<br>4x4= " + multi)
+// var multi =(4)
+// multi*=5
+// document.write("<br>4x5= " + multi)
+// var multi =(4)
+// multi*=6
+// document.write("<br>4x6= " + multi)
+// var multi =(4)
+// multi*=7
+// document.write("<br>4x7= " + multi)
+// var multi =(4)
+// multi*=8
+// document.write("<br>4x8= " + multi)
+// var multi =(4)
+// multi*=9
+// document.write("<br>4x9= " + multi)
+// var multi =(4)
+// multi*=10
+// document.write("<br>4x10= " + multi)
+// QUESTION NO 6
+// document.write("Conversation Formulae:")
+// var age =("<br>C = (F-32)*5/9")
+// document.write(age)
+// var age =("<br>F = (C-9/5)+32")
+// document.write(age)
+// QUESTION NO 7
+// document.write("<h1>Shopping Cart</h1><br> a. Price of item 1 is ");
+// var a = (650);
+// document.write(a)
+// document.write("<br> b. Quantity of item 1 is  ");
+// var b = (3);
+// document.write(b)
+// document.write("<br> c. Price of item 2 is  ");
+// var c = (100);
+// document.write(c)
+// document.write("<br> d. Quantity of item 2 is  ");
+// var d = (7);
+// document.write(d)
+// document.write("<br> e. Shipping charges ");
+// var e = (100);
+// document.write(e)
+// document.write("<br><br>Total cost of your order is ")
+// var sum = a + b + c + d + e;
+// document.write(sum)
+// document.write("<h1>Marks Sheet</h1> ");
+// document.write("<br><br> Total Marks: 980 ");
+// var total = (980);
+// document.write("<br>obtained Marks: 804 ");
+// var obt = (804);
+// document.write("<br>Percentage " + obt/total*100)
+// QUESTION NO 9
+// document.write("<h1>Currency in PKR</h1>");
+// var num =(104.80);
+// num*=10
+// var num2 =(28)
+// num2*=25
+// document.write(num+num2)
+
+
+
+// QUESTION NO 10
+// var num =(15);
+// var age  =((num + 5)*10 /2)
+// document.write(age)
+// QUESTION NO 11
+// document.write("<h1>Age Calculator</h1>");
+// document.write("<br><br> Current Year: 2016 ")
+// var a = 2016;
+// document.write(" <br>Birth Year: 1992")
+
+//  var b = 1992;
+//  document.write("<br>Your age is; " - b - a)
+// QUESTION NO 12
+
+// CHAPTER NO 6-9
+// QUESTION NO 1
+1
+// var head =("Result:<br> The value of a is 10;<br>....................................<br> <br> ")
+// document.write(head)
+// document.write("The value of ++a is:")
+// var a =(10);
+// document.write(++a)
+// document.write("<br>Now the value of a is: 11")
+
+// document.write("<br><br>The value of a++ is: ")
+// var b =(11);
+// document.write(a++)
+// document.write("<br>Now the value of a is: ")
+// var b =(12);
+// document.write(a++)
+// document.write("<br><br>The value of --a is: ")
+// var a =(12);
+// document.write(--a)
+// document.write("<br> Now the value of a is: ")
+// var a =(11);
+// document.write(a)
+// document.write("<br><br>The value of a-- is: ")
+// var a =(11);
+// document.write(a--)
+// document.write("<br>Now the value of a-- is: ")
+// var a =(10);
+// document.write(a--)
+// QUESTION NO 2
+// var a =(2);
+// var b =(1);
+// console.log("--a - --b + ++b +b--");
+// console.log(1 - 0 + 2 + 1)
+// QUESTION NO 3
+// var userInput =(promt);
+// QUESTION NO 5
+// Number();
+// parseInt();
+// +
+// QUESTION NO 6
+document.write("<h5>Subjest Total Marks Obtained Marks Percentage</h5>")
+var sub1 =("English &nbsp &nbsp&nbsp &nbsp ")
+var num1 =(100 )
+var num4 =(54)
+
+document.write(sub1 ," " ," "+ num1 , "  " + num4, " " +num4/num1*100)
+var sub2 =("<br>Maths &nbsp &nbsp &nbsp&nbsp&nbsp&nbsp")
+var num2 =(100)
+var num5 =(54)
+document.write(sub2 ," "+ " "+ " " + num2 ," "+""+ num5," "+num5/num2*100)
+var sub3 =("<br>Urdu &nbsp&nbsp &nbsp &nbsp&nbsp&nbsp&nbsp&nbsp")
+var num3 =(100)
+var num6 =(48)
+document.write(sub3 ," "+ " "+ " " + num3 ," "+num6, " "+num6/num3*100)
+document.write("<br>Total Marks " + num1*3 )
+document.write("<br>Obtained Marks "+ num4*3)
+document.write("<br>Percentage" + num4/num1*100)
